e8e14966e47ed1ae84974e57e4f8ff640ca4f1e3
Tony Anziano <toanzian@microsoft.com>
2019-09-18 14:58:11 -0700

    Working static version with busted z-index.

--- packages/app/client/src/ui/shell/appMenu/appMenu.scss.d.ts
+++ packages/app/client/src/ui/shell/appMenu/appMenu.scss.d.ts
@@ -1,2 +1,4 @@
 // This is a generated file. Changes are likely to result in being overwritten
 export const appMenu: string;
+export const menu: string;
+export const menuItem: string;
--- packages/app/client/src/ui/shell/appMenu/appMenu.tsx
+++ packages/app/client/src/ui/shell/appMenu/appMenu.tsx
@@ -35,6 +35,24 @@ import * as React from 'react';
 
 import * as styles from './appMenu.scss';
 
+type MenuItemType = 'default' | 'checkbox' | 'submenu' | 'separator';
+interface MenuItem {
+  disabled?: boolean;
+  items?: MenuItem[];
+  label?: string;
+  type: MenuItemType;
+}
+
+const fileMenu: MenuItem[] = [
+  { type: 'default', label: 'New bot config...' },
+  { type: 'separator' },
+  {
+    label: 'Themes',
+    type: 'submenu',
+    items: [{ type: 'default', label: 'Light' }, { type: 'default', label: 'Dark' }],
+  },
+];
+
 export interface AppMenuProps {}
 
 export interface AppMenuState {}
@@ -42,14 +60,83 @@ export interface AppMenuState {}
 export class AppMenu extends React.Component<AppMenuProps, AppMenuState> {
   public render(): React.ReactNode {
     return (
-      <ul className={styles.appMenu}>
-        <li>File</li>
-        <li>Debug</li>
-        <li>Edit</li>
-        <li>View</li>
-        <li>Conversation</li>
-        <li>Help</li>
+      <>
+        <ul className={styles.appMenu}>
+          <li>File</li>
+          <li>Debug</li>
+          <li>Edit</li>
+          <li>View</li>
+          <li>Conversation</li>
+          <li>Help</li>
+        </ul>
+        <Menu items={fileMenu}></Menu>
+      </>
+    );
+  }
+}
+
+export interface MenuProps {
+  items: MenuItem[];
+  showing?: boolean;
+}
+export interface MenuState {
+  showing: boolean;
+}
+export class Menu extends React.Component<MenuProps, MenuState> {
+  constructor(props: MenuProps) {
+    super(props);
+    this.state = {
+      showing: true,
+    };
+  }
+
+  public render(): React.ReactNode {
+    const { items = [] } = this.props;
+
+    if (!this.state.showing) {
+      return null;
+    }
+
+    return (
+      <ul className={styles.menu}>
+        {items.map(item => {
+          if (item.type === 'default') {
+            return <MenuItem>{item.label}</MenuItem>;
+          } else if (item.type === 'separator') {
+            return <MenuItem>======</MenuItem>;
+          } else if (item.type === 'submenu') {
+            return <SubMenu items={item.items} label={item.label}></SubMenu>;
+          }
+        })}
       </ul>
     );
   }
 }
+
+export class SubMenu extends React.Component<MenuProps & { label: string }, MenuState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      showing: false,
+    };
+  }
+
+  public render(): React.ReactNode {
+    return (
+      <li className={styles.menuItem} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
+        {this.props.label}
+        {this.state.showing && <Menu showing={this.state.showing} items={this.props.items}></Menu>}
+      </li>
+    );
+  }
+
+  public onMouseEnter = (): void => {
+    this.setState({ showing: true });
+  };
+
+  public onMouseLeave = (): void => {
+    this.setState({ showing: false });
+  };
+}
+
+export const MenuItem = props => <li>{props.children}</li>;
